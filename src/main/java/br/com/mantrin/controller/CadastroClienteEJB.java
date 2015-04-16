67f45fbc1f2d821f48535bf15795da6da6606d3e
P001258 <P001258@GEDES-P001258.prodam.local>
2015-04-16 14:20:34 -0400

    Projeto de Teste

---
+++ src/main/java/br/com/mantrin/controller/CadastroClienteEJB.java
@@ -0,0 +1,26 @@
+package br.com.mantrin.controller;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.mantrin.modell.Cliente;
+
+@Stateless
+public class CadastroClienteEJB {
+
+	@PersistenceContext(unitName="projetoJbossPU")
+	private EntityManager em;
+	
+	public void salvar(Cliente cliente) {
+		em.persist(cliente);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Cliente> buscarTodos() {
+		return em.createQuery("from Cliente c").getResultList();
+	}
+	
+}
---
+++ src/main/java/br/com/mantrin/modell/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.mantrin.modell;
+
+public class Usuario {
+	
+	private double id;
+	private String nome;
+	
+	
+	public double getId() {
+		return id;
+	}
+	public void setId(double id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+
+}
